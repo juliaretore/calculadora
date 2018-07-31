0da2a42b71231b5b23d82c6f1fe7f8191a12a186
fabricneiisabella <fabricneiisabella@gmail.com>
2018-07-31 08:36:38 -0300

    alt2

---
+++ Calc/src/ifc/videira/edu/br/Main.java
@@ -0,0 +1,13 @@
+package ifc.videira.edu.br;
+
+public class Main {
+	public static void main (String args[]){ 
+       
+        Calculadora c = new Calculadora();  
+       
+        int opcao = 5;  
+        int num1;  
+        int num2;
+
+}
+}
