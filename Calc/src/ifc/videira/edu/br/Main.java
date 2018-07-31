1c028c827d14c2ad62f3db7316eec1f4ab9ee030
juliaretore <juliaretore@hotmail.com>
2018-07-31 08:44:59 -0300

    criação dos system out print

--- Calc/src/ifc/videira/edu/br/Main.java
+++ Calc/src/ifc/videira/edu/br/Main.java
@@ -1,4 +1,5 @@
 package ifc.videira.edu.br;
+import java.util.Scanner;
 
 public class Main {
 	public static void main (String args[]){ 
@@ -8,6 +9,16 @@ public class Main {
         int opcao = 5;  
         int num1;  
         int num2;
+        
+        Scanner input = new Scanner(System.in);  
+		System.out.println("-Escolha uma opção-");
+	    System.out.println("1. Soma");  
+	    System.out.println("2. Subtracao");  
+	    System.out.println("3. Multiplicacao");  
+	    System.out.println("4. Divisao");  
+	    System.out.println("0. Sair");  
+	    System.out.println("Operação: ");  
+	    opcao = input.nextInt();
 
 }
 }
