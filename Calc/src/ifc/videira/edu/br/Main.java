fc85bed2ba92ad25d98b012b923d2f7e49428a02
juliaretore <juliaretore@hotmail.com>
2018-08-07 08:14:20 -0300

    mult. e correção de erros

--- Calc/src/ifc/videira/edu/br/Main.java
+++ Calc/src/ifc/videira/edu/br/Main.java
@@ -21,12 +21,13 @@ public class Main {
 	    opcao = input.nextInt();
 
 	    while (opcao != 0) {
-			Scanner input1 = new Scanner(System.in);    
-			System.out.println("Qual o primeiro numero: ");  
-			num1 = input1.nextInt();  
-			System.out.println("Qual o segundo numero: ");  
-			num2 = input1.nextInt();
+
 			if ( opcao == 1 ) {
+				Scanner input1 = new Scanner(System.in);    
+				System.out.println("Qual o primeiro numero: ");  
+				num1 = input1.nextInt();  
+				System.out.println("Qual o segundo numero: ");  
+				num2 = input1.nextInt();
 				int operacao = c.som(num1, num2);
                 System.out.printf("\nO resultado da soma é: %d\n", operacao);  
                 break;  
@@ -41,6 +42,17 @@ public class Main {
 	    			System.out.println(operacao);
 	    		break;
 	    		}
+			if (opcao == 3) {
+  	 		  Scanner input1 = new Scanner(System.in);  
+	    		System.out.println("Qual o primeiro numero: ");
+	    		 num1 = input1.nextInt();
+	    		System.out.println("Qual o segundo numero: ");
+	    		 num2 = input1.nextInt();
+	    			int operacao = c.mult(num1, num2);
+	    			System.out.println(operacao);
+	    		break;
+  	 	}
+			
 	    }
 	  }
 }
